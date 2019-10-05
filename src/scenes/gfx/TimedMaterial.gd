f222bea2972e9fb89e877f4db121f5bcca934759
Scoba <anton@doridaweb.com>
2019-10-05 13:44:23 +0700

    Star and bubble gfx, project settings

---
+++ src/scenes/gfx/TimedMaterial.gd
@@ -0,0 +1,19 @@
+extends MeshInstance
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var material
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	material = get_surface_material(0);
+
+var time = 0.0;
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	time += delta;
+	# var material = mesh.surface_get_material(0);
+	material.set_shader_param("time", time);
+
