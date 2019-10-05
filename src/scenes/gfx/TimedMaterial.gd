0afe8f871ea020895db61a78a5a19929b8e3797e
Scoba <anton@doridaweb.com>
2019-10-05 21:09:16 +0700

    Two lights rotating

--- src/scenes/gfx/TimedMaterial.gd
+++ src/scenes/gfx/TimedMaterial.gd
@@ -7,14 +7,32 @@ extends MeshInstance
 var material
 
 # Called when the node enters the scene tree for the first time.
+var light;
+var lightDist;
+
+var light2;
+var lightDist2;
+
 func _ready():
 	material = get_surface_material(0);
+	
+	light = get_node("../OmniLight");
+	lightDist = light.translation.length();
+	
+	light2 = get_node("../OmniLight2");
+	lightDist2 = light2.translation.length();
+	
 
 var time = 0.0;
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+
+func moveLight():
+	light.translation = Vector3(lightDist * sin(time), lightDist * cos(time), 0.0);
+	light2.translation = Vector3(lightDist2 * sin(time*3.0), 0.0, lightDist2 * cos(time*2.0));
 
 func _process(delta):
 	time += delta;
 	# var material = mesh.surface_get_material(0);
 	material.set_shader_param("time", time);
+	moveLight();
 
