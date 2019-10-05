c22f143b5dfbbe22bdeac13c7f453d8e8d56a8d8
Anatoly Griko <anhel.strauke@gmail.com>
2019-10-05 19:56:48 +0700

    Added items auto-gen

--- src/scenes/game/level/AutoGenLevel.gd
+++ src/scenes/game/level/AutoGenLevel.gd
@@ -1,6 +1,8 @@
 extends Spatial
 
 var tiles = []
+var real_level_length: float = 0.0
+var items = []
 
 export var level_length: float = 0.0
 
@@ -10,10 +12,14 @@ onready var tile_resources = [
 	preload("res://scenes/game/level/tiles/test_tile_3.tscn"),
 ]
 
+onready var item_resources = [
+	preload("res://scenes/game/level/objects/RedSphere.tscn"),
+]
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if level_length > 0.0:
-		generate_level(level_length)
+		generate_level(level_length, 25)
 
 
 func update_visibility(from_pos: float, screen_w: float):
@@ -25,18 +31,35 @@ func update_visibility(from_pos: float, screen_w: float):
 		cur_pos += tile.tile_width
 
 
-func generate_level(length: float):
+func _make_floor(length: float):
 	for tile in tiles:
 		self.remove_child(tile)
 		tile.queue_free()
 	tiles = []
-	var	curr_length = 0.0
-	while curr_length < length:
+	real_level_length = 0.0
+	while real_level_length < length:
 		var res: Resource = tile_resources[randi() % len(tile_resources)]
 		var tile: Spatial = res.instance()
 		self.add_child(tile)
-		tile.translation = Vector3(curr_length + tile.tile_width / 2, 0, 0)
-		curr_length += tile.tile_width
+		tile.translation = Vector3(real_level_length + tile.tile_width / 2, 0, 0)
+		real_level_length += tile.tile_width
+
+
+func _make_items(items_count: int):
+	for item in items:
+		self.remove_child(item)
+		item.queue_free()
+	for i in range(items_count):
+		var new_item: Spatial = item_resources[0].instance()
+		var z_pos = rand_range(-3.5, 3.5)
+		var x_pos = rand_range(0.0, real_level_length)
+		self.add_child(new_item)
+		new_item.translation = Vector3(x_pos, 1.0, z_pos)
+
+
+func generate_level(length: float, items: int):
+	_make_floor(length)
+	_make_items(items)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
