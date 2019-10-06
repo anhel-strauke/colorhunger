06f4554dd7ff301bdcc94a4df458cc98be1b0558
Scoba <anton@doridaweb.com>
2019-10-06 11:17:52 +0700

    Color shapes

--- src/scenes/game/level/objects/ColorItem.gd
+++ src/scenes/game/level/objects/ColorItem.gd
@@ -6,6 +6,12 @@ extends Spatial
 
 export (Color) var color = Color(1.0, 0.0, 0.0)
 
+var color_classes = [
+	preload("res://scenes/gfx/Color1.tscn"),
+	preload("res://scenes/gfx/Color2.tscn"),
+	preload("res://scenes/gfx/Color3.tscn")
+]
+
 signal kill_me(item)
 
 func affect_hero(hero, direction):
@@ -20,11 +26,30 @@ func suicide():
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_color(color)
+	pass
+	# set_color(color)
 
+func create_model():
+	var model;
+	model = $Model
+	remove_child(model)
+	model.free()
+	
+	if color.r > 0.0:
+		model = color_classes[0].instance()
+	elif color.g > 0.0:
+		model = color_classes[1].instance()
+	elif color.b > 0.0:
+		model = color_classes[2].instance()
+	else:
+		raise()
+	model.name = "Model"
+	add_child(model)
 
 func set_color(col: Color):
 	color = col
+	create_model()
+	
 	$Model/MeshInstance.setColor(Vector3(color.r, color.g, color.b), 1.0);
 	return
 	var mat = SpatialMaterial.new()
